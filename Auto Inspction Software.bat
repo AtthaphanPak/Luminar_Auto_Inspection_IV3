5f5e95b8c89d8bbf3567e60fc747bb72c7a05f57
atthaphanpak <atthaphanpaksakunnee@gmail.com>
2025-07-24 11:38:33 +0700

    debug call python in bat file

--- Auto Inspction Software.bat
+++ Auto Inspction Software.bat
@@ -1,4 +1,4 @@
 @echo off
 cd C:\Projects\AOI_SCANNER\Sources
-start "" ""C:\Users\Atthaphanp\AppData\Local\Programs\Python\Python39\pythonw.exe"" "Main_window.py"
+start "" ""%USERPROFILE%\AppData\Local\Programs\Python\Python39\pythonw.exe"" "Main_window.py"
 exit
